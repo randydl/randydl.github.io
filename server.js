f08db95c4452aa073adcdbe65b9a7cb647e2eddd
Randy <hui.kang.future@foxmail.com>
2023-08-23 13:39:35 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+const port = process.argv[2] || 6006;
 
 app.use(express.static(__dirname));
-app.listen(6006, () => { console.log('Hello World'); });
+app.listen(port, () => { console.log(`Server started on port ${port}`); });
