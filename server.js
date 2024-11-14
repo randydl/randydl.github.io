d27df808e5dcee5bfbd1f83968fc735ba3df408c
Randy <hkan8178@qq.com>
2024-11-15 03:16:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+import path from 'path';
 import express from 'express';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.argv[2] || 8000;
